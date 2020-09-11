8c2220093a1c9107914dc0299153a958ccfffc7c
poiutes <rystuard@gmail.com>
2020-09-11 00:10:48 -0500

    jkh;kh

--- training_analysis.py
+++ training_analysis.py
@@ -2,6 +2,7 @@ import nn
 import torch
 import feed
 import pandas
+import os
 
 net = nn.Net ()
 
@@ -19,6 +20,17 @@ def percent_right (ins,eos,raw1):
 	
 	correct = 0
 	total = 0
+
+	
+	os.remove ('bank_price.csv') 
+	os.remove ('performance.csv') 
+	os.remove ('correct_trades.csv')
+
+	f1 = open ('bitcoin_price.csv','a')
+	f2 = open ('bank_price.csv','a')
+	f3 = open ('performance.csv','a')
+	f4 = open ('correct_trades.csv','a')
+
 	with torch.no_grad (): 
 		i = 0
 		while i != len (dataset):
@@ -34,27 +46,50 @@ def percent_right (ins,eos,raw1):
 			
 			if outputs[0][0] <= outputs[0][1]:
 				if eos[0][1] == 1.0:
-					profit = raw[0][14]/raw[0][9]
+					profit = raw[i][14]/raw[i][9]
 					bank = bank*profit
+					
 					correct = correct + 1
 
 				if eos[0][1] == 0.0:
 					
-					profit = raw[0][14]/raw[0][9]
+					profit = raw[i][14]/raw[i][9]
 					bank = bank*profit
+					
 			
+
 			total = total + 1
+
+			percent = correct/total
+			percent = percent*100
+			percent = round (percent,4)
+			percent_str = str (percent)
+			performance = bank/raw[i][14]
+			performance_str = str (performance)
+			market = str (raw[i][14])
+			bank_str = str (bank)
+
+			f1.write (market)
+			f1.write (',')
+			f2.write (bank_str)
+			f2.write (',')
+			f3.write (performance_str)
+			f3.write (',')
+			f4.write (percent_str)
+			f4.write (',')
+
 			i = i + 1
+	f1.close ()
+	f2.close ()
+	f3.close ()
+	f4.close ()
 
 	
-	percent = correct/total
-	percent = percent*100
+	
 	print ('percent_right:',percent,'%')
 	print ('bank:',bank)
-	market = raw[99][14]
 	print ('market:',market)
 	print ('market start',raw[0][0])
-	performance = bank/market
 	print ('performance:',performance)
 	percent_return = bank/raw[0][0]
 	print ('percent return:',percent_return)
